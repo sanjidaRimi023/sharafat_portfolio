0fd0b2ab01572cd2717e5ccd41fed7b90c941037
sanjidaRimi023 <sanjidarimi023@gmail.com>
2025-10-24 21:21:28 +0600

    added theme and toggle button

---
+++ src/app/components/mode-toggle.tsx
@@ -0,0 +1,60 @@
+"use client";
+import * as React from "react";
+import { Moon, Sun } from "lucide-react";
+import { useTheme } from "next-themes";
+import { Button } from "@/components/ui/button";
+import { AnimatePresence } from "framer-motion";
+import { motion} from 'framer-motion';
+
+export function ModeToggle() {
+  const { theme, setTheme, systemTheme } = useTheme();
+  const [mounted, setMounted] = React.useState(false);
+
+  React.useEffect(() => {
+    setMounted(true);
+  }, []);
+  if (!mounted) {
+    <Button variant="outline" size="icon" disabled>
+      <Sun className="h-[1.2rem] w-[1.2rem]" />
+    </Button>;
+  }
+  const currentTheme = theme === "system" ? systemTheme : theme;
+
+  const toggleTheme = () => {
+    setTheme(currentTheme === "dark" ? "light" : "dark");
+  };
+
+  return (
+  <Button
+      variant="outline"
+      size="icon"
+      onClick={toggleTheme}
+      className="relative overflow-hidden transition-all cursor-pointer rounded-full"
+    >
+      <AnimatePresence mode="wait" initial={false}>
+        {currentTheme === "dark" ? (
+          <motion.div
+            key="sun"
+            initial={{ rotate: -90, scale: 0 }}
+            animate={{ rotate: 0, scale: 1 }}
+            exit={{ rotate: 90, scale: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <Sun className="h-[1.2rem] w-[1.2rem]" />
+          </motion.div>
+        ) : (
+          <motion.div
+            key="moon"
+            initial={{ rotate: 90, scale: 0 }}
+            animate={{ rotate: 0, scale: 1 }}
+            exit={{ rotate: -90, scale: 0 }}
+            transition={{ duration: 0.3 }}
+          >
+            <Moon className="h-[1.2rem] w-[1.2rem] text-primary" />
+          </motion.div>
+        )}
+      </AnimatePresence>
+      <span className="sr-only">Toggle theme</span>
+    </Button>
+  );
+}
